f0085dc11ffa88f265ee5fe8fd3d741ed43bd213
Ali Jawad <97254328+AliJawadSubhan@users.noreply.github.com>
2025-11-04 23:59:10 +0500

    'portfolio'

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from './global_components/navbar'
+// import tailwindcss from '@tailwindcss/vite'
+const App = () => {
+  return (
+    <div >
+      <div className="sticky top-0 z-50 h-40 w-1 bg-black pl-12"></div>
+
+      <Navbar />
+       <Navbar />
+        <Navbar />
+         <Navbar />
+          <Navbar />
+    </div>
+
+  )
+}
+
+export default App
